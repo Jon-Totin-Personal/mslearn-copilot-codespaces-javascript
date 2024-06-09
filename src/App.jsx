4098c4be780458d834d382ffe207f7ba9063adad
Jon-Totin-Personal <littlet_email@yahoo.com>
2024-06-09 17:42:01 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Jon Totin",
   title: "Web Designer & Content Creator",
   email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  gitHub: "https://github.com/",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/jonathan-totin-02269370/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
